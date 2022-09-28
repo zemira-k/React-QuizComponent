f4c19567f8edd82165938135b31184d657a043c6
zemira kehati <kehati10@gmail.com>
2022-09-29 01:14:25 +0300

    Quiz Questions Handling Events

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,6 +9,9 @@ class Quiz extends Component {
     super(props);
     this.state = { quiz_position: 1 };
   }
+  showNextQuestion() {
+    this.setState({ quiz_position: this.state.quiz_position + 1 });
+  }
   render() {
     const isQuizEnd =
       this.state.quiz_position - 1 === quizData.quiz_questions.length;
@@ -21,6 +24,7 @@ class Quiz extends Component {
             quiz_question={
               quizData.quiz_questions[this.state.quiz_position - 1]
             }
+            showNextQuestionHandler={this.showNextQuestion.bind(this)}
           />
         )}
       </div>
