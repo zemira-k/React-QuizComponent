77bd3c8e44996a73eecf748ef2c7fad7eebe4886
zemira kehati <kehati10@gmail.com>
2022-09-29 01:31:59 +0300

    Resetting the Quiz

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,13 +12,16 @@ class Quiz extends Component {
   showNextQuestion() {
     this.setState({ quiz_position: this.state.quiz_position + 1 });
   }
+  handleResetClick() {
+    this.setState({ quiz_position: 1 });
+  }
   render() {
     const isQuizEnd =
       this.state.quiz_position - 1 === quizData.quiz_questions.length;
     return (
       <div>
         {isQuizEnd ? (
-          <QuizEnd />
+          <QuizEnd resetClickHandler={this.handleResetClick.bind(this)} />
         ) : (
           <QuizQuestion
             quiz_question={
